eb54c20061e0df40a3729318facd63ad0b28e622
lin559 <scau_ljw@126.com>
2022-01-18 19:42:12 +0800

    update: new func a

--- packages/core/lib/core.js
+++ packages/core/lib/core.js
@@ -6,4 +6,9 @@ function core() {
     // TODO
     console.log('something');
     console.log('version2.0.0');
+    let a = function () {
+        console.log('a function');
+        return new Date();
+    }
+    return a;
 }
