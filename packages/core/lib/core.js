7cd2d5cc669a92461e95ee70bd016ed71800aac5
lin559 <scau_ljw@126.com>
2022-01-20 16:28:35 +0800

    fix: fix some bugs

--- packages/core/lib/core.js
+++ packages/core/lib/core.js
@@ -16,6 +16,8 @@ function core() {
         console.log('feat: dd');
         console.log('breaking change: 01');
         console.log('fix bug');
+        console.log('fix bug');
+        console.log('fix bug');
         return new Date();
     }
     return a;
